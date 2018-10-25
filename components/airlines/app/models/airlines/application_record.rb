8efc64bea66394e035fa92dbee03b7ac9b23f389
Ben Langfeld <blangfeld@powerhrg.com>
2018-10-25 16:27:41 -0300

    WIP: Play around with multiple databases

--- components/airlines/app/models/airlines/application_record.rb
+++ components/airlines/app/models/airlines/application_record.rb
@@ -1,5 +1,10 @@
 module Airlines
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+    db_name_file = File.join(Rails.root, "db/name")
+    if File.exists?(db_name_file)
+      db_name = File.read(db_name_file).chomp
+      establish_connection db_name
+    end
   end
 end
--- components/destinations/app/models/destinations/application_record.rb
+++ components/destinations/app/models/destinations/application_record.rb
@@ -1,5 +1,10 @@
 module Destinations
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+    db_name_file = File.join(Rails.root, "db/name")
+    if File.exists?(db_name_file)
+      db_name = File.read(db_name_file).chomp
+      connects_to database: { reading: db_name, writing: db_name }
+    end
   end
 end
